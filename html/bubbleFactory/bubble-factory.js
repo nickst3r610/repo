525398545c555d49548e316075fc64bf2645546c
nickst3r610 <nicholaschavez@ac.c4q.nyc>
2017-11-02 10:53:39 -0400

    edit

--- html/bubbleFactory/bubble-factory.js
+++ html/bubbleFactory/bubble-factory.js
@@ -1,8 +1,27 @@
-document.addEventListener("click", function(event) {
-   var img = document.createElement("img")
-   var body = document.querySelector("body")
-   if (event.target === document.querySelector('button')) {
-       img.src= "bubble.png"
-       body.appendChild(img)
-   }
+// document.addEventListener("click", function(event) {
+//    var img = document.createElement("img")
+//    var body = document.querySelector("body")
+//    if (event.target === document.querySelector('button')) {
+//        img.src= "https://slack-imgs.com/?c=1&url=http%3A%2F%2Fwww.freeiconspng.com%2Fuploads%2Fbubbles-png-22.png"
+//        img.style.width = "50px"
+//        body.appendChild(img)
+//    }
+// })
+
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    let button = document.querySelector("button");
+
+    button.addEventListener("click", function(event) {
+        console.log( Math.round(Math.random() * 100 ));
+        let bubble = document.createElement("img");
+        bubble.src = "https://slack-imgs.com/?c=1&url=http%3A%2F%2Fwww.freeiconspng.com%2Fuploads%2Fbubbles-png-22.png";
+        bubble.style.height = Math.round(Math.random() * 50 + 10 ).toString() + "px";
+        bubble.style.top = Math.round(Math.random() * 95 ).toString() + "%";
+        bubble.style.left = Math.round(Math.random() * 99 ).toString() + "%";
+        bubble.style.position = "fixed";
+
+        document.body.appendChild(bubble);
+    })
 })
--- html/quadrant-toggle/quadrant-toggle.js
+++ html/quadrant-toggle/quadrant-toggle.js
@@ -1,8 +1,14 @@
+// document.addEventListener('click', function(event) {
+//     var target = event.target.style
+//    if (target.backgroundColor === "white") {
+//        target.backgroundColor = "black"
+//    } else {
+//        target.backgroundColor = "white"
+//    }
+// })
+
+
 document.addEventListener('click', function(event) {
-    var target = event.target
-   if (target.style.backgroundColor === "white") {
-       target.style.backgroundColor = "black"
-   } else {
-       target.style.backgroundColor = "white"
-   }
+    var target = event.target.style
+    target.backgroundColor = target.backgroundColor === "lightblue" ? "salmon" : "lightblue"
 })
