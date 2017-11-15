5b28aabc63e69e69dacc59a224a4d5f02a428a9f
nickst3r610 <nicholaschavez@ac.c4q.nyc>
2017-11-15 13:36:59 -0500

    npm exercises

--- html/npm/trychalk/add.js
+++ html/npm/trychalk/add.js
@@ -1,18 +1,59 @@
 const chalk = require('chalk');
 
-
+//Exercise 1//
 let helloBlue = (string) => {
     console.log(chalk.blue(string));
 };
 
-
+//Exercise 2//
 let helloRed = (string) => {
     console.log(chalk.red(string));
 };
 
+//Exercise 3//
 let stringToColor = (string, color) => {
-    let typeColor = color
-    console.log(chalk[.typeColor](string));
+    console.log(chalk[color](string));
 };
 
-stringToColor('hi','red')
+//Exercise 4//
+let evensBlueOddsYellow = (string) => {
+    let arr = string.split(" ")
+    let newArr = []
+    
+    for (var i = 0; i < arr.length; i++) {
+        if (i % 2 === 0) {
+            newArr.push(chalk.yellow(arr[i]))
+        } else {
+            newArr.push(chalk.blue(arr[i]))
+        }
+    }
+    let newString = newArr.join(" ")
+    console.log(newString)
+}
+
+//Exercise 5
+let angryText = (string) => {
+    console.log(chalk.red.underline.bold(string.toUpperCase()))
+}
+
+//Exercise 6
+let backgroundCyan = (string) => {
+    console.log(chalk.white.bgCyan(string))
+}
+
+//Exercise 7
+let boldFirstUnderlineLast = (string) => {
+    let arr = string.split(" ")
+    let newArr = []
+
+    for (var i = 0; i < arr.length; i++) {
+        if (i === 0) {
+            newArr.push(chalk.bold(arr[0]))
+        } else if (i === (arr.length-1)) {
+            newArr.push(chalk.underline(arr[arr.length-1]))
+        } else {
+            newArr.push(arr[i])
+        } 
+    }
+    console.log(newArr.join(" "))
+}
